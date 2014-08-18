f663089677b74476518b6450d9fcec10edf738d3
Luis Lavena <luislavena@gmail.com>
2014-08-18 12:42:54 -0300

    Show the version of ruby-install that was just installed

--- scripts/install.osx.deps.sh
+++ scripts/install.osx.deps.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 set -ev
 
-install() {
-  # install ruby-install using Homebrew
-  brew update
-  which ruby-install || brew install ruby-install
-}
+# install ruby-install using Homebrew
+brew update
+which ruby-install || brew install ruby-install
 
-install
+ruby-install --version
